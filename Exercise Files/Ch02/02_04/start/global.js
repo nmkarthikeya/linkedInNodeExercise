3c2a9936170899fcbaa609c537297d50a1045c93
Karthikeya NM <nmkarthikeya@gmail.com>
2023-10-31 14:31:26 +0530

    week 2, 4th and 5th exercise

--- Exercise Files/Ch02/02_04/start/global.js
+++ Exercise Files/Ch02/02_04/start/global.js
@@ -1,10 +1,20 @@
-function grab(flag) {
-  let indexAfterFlag = process.argv.indexOf(flag) + 1;
-  return process.argv[indexAfterFlag];
+process.stdout.write("Hello ")
+
+const questions = [
+"What is your name",
+"What do you want to be",
+"What language do you like",
+];
+
+const answers = [];
+
+function ask(i){
+    process.stdout.write(`\n\n ${questions[i]} `);
+    process.stdout.write(` > `);
 }
 
-let greeting = grab("--greeting");
-let user = grab("--user");
+process.stdin.on("data",function(data){
+    process.stdout.write(data.toString().trim());
+});
 
-console.log(greeting);
-console.log(user);
+ask(answers.length);
