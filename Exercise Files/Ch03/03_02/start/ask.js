6c5cbb08360cd38cd145c21af818ba791f8ea8ee
Karthikeya NM <nmkarthikeya@gmail.com>
2023-11-22 11:44:21 +0530

    working with readline

---
+++ Exercise Files/Ch03/03_02/start/ask.js
@@ -0,0 +1,10 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+rl.question("how do you like Node?", (answer) => {
+    console.log(`your answer ${answer}`);
+} );
--- Exercise Files/Ch03/03_03/start/ask.js
+++ Exercise Files/Ch03/03_03/start/ask.js
@@ -4,6 +4,32 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("How do you like Node?", (answer) => {
-  console.log(`Your answer: ${answer}`);
-});
+const questions = [
+  "what is your name",
+  "Where do you live",
+  "what are you going to do with Node js"
+];
+
+function collectAnswers( questions, done ){
+  const answers = [];
+
+  const questionsAnswered = (answer) => {
+
+     answers.push(answer.trim());
+
+     if(answers.length < questions.length){
+      rl.question(questions[answers.length],
+      questionsAnswered );
+     } else {
+      return done(answers);
+     }
+
+  }
+  rl.question(questions[0],questionsAnswered);
+}
+
+collectAnswers( questions, (answers) => {
+  console.log("Thank you for your answers");
+  console.log(answers);
+  process.exit();
+} )
